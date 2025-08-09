51ab47a16090ab0312e040ac8cb5f777260ed3ab
Anton Golubchikov <anton1programmist@gmail.com>
2025-08-09 12:38:27 +0700

    fx for ruff and mypy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,19 @@
+"""Create a fixture for the app."""
+
 import pytest
 from fastapi.testclient import TestClient
+
 from metrichub import app
 
 
 @pytest.fixture
-def client():
+def client() -> TestClient:
+    """
+    Client craete a test client for the app.
+
+    Returns
+    -------
+    TestClient
+        Test client for the app.
+    """
     return TestClient(app)
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,2 +1,15 @@
-def test_simple(client):
+"""Simple test to check if the server is up and running."""
+
+from fastapi.testclient import TestClient
+
+
+def test_simple(client: TestClient) -> None:
+    """
+    test_simple check if the server is up and running.
+
+    Parameters
+    ----------
+    client : TestClient
+        The client used to make the request to the server.
+    """
     assert client.get("/").status_code == 200
